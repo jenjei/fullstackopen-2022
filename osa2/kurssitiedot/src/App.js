4d02459d3ed973bfe3d760ea8c8e7c008c4c3490
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-10-24 17:27:26 +0300

    Exercise 2.2 and 2.3* with console log

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -4,26 +4,37 @@ const Course = ({ course }) =>
 <>
 <Header course={course.name} />
 <Content parts={course.parts} />
+<Total parts={course.parts}/>
 </>
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => 
-  <p>
-    Number of exercises {sum}
-  </p>
+const Total = ({ parts }) => {
+  // reduce function counts the sum of array values conveniently
+  const sum = parts.reduce((previous, current) => console.log(previous, current) || (previous = previous + current.exercises), 0)
+  console.log('total sum', sum)
+
+  return (
+    <b>
+      Number of exercises {sum}
+    </b>
+  )
+}
 
 const Part = ({ part }) => 
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) => {
+  console.log('content', parts)
   // mapping props so that no matter how many parts added/deleted in App component, parts will render correctly on screen
+  return(
   <>
     {parts.map(partz => <Part key={partz.id} part={partz}/>)}
   </> // parts and partz are different!! parts is props and partz mapping values
-
+  )
+}
 
 
 const App = () => {
