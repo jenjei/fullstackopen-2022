9c1e205bd5c3d4ccc898384875f6b9ada0536354
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-10-27 18:45:35 +0300

    Exercise 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleAddClick = (event) => {
     event.preventDefault() // preventing the page to refresh, not always necessary but keep this in mind :)
@@ -22,15 +23,21 @@ const App = () => {
 
     const nameObject = {
       name: newName,
+      number: newNumber,
     }
     setPersons(persons.concat(nameObject))
     console.log('names: ', {persons})
     setNewName('') // clearing the input field
   }
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -39,8 +46,9 @@ const App = () => {
 
       <form onSubmit={handleAddClick}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -48,7 +56,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>debug: {newName}</div>
-      {persons.map((person, id) => <p key={id}> {person.name}</p>)}
+      {persons.map((person, id) => <p key={id}> {person.name} {person.number}</p>)}
     </div>
   )
 
