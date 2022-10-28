dd1feedffe2eab1cc54b861edfe864dee434f296
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-10-28 13:07:01 +0300

    Exercise 2.10. Tried to do 2.9* but I didnt know how.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,43 @@
 
 import { useState } from 'react'
 
+const Person = ({person}) => {
+  return (
+    <ul>{person.name} {person.number}</ul>
+  )
+}
+
+const Contacts =({persons}) => {
+  return (
+    <div>
+        {persons.map((persons, id) => 
+          <Person key={id} person={persons} />
+          )}
+    </div>
+  )
+}
+
+const PersonForm = ({handleAddClick, newName, newNumber, handleNameChange, handleNumberChange}) => {
+  return (
+    <form onSubmit={handleAddClick}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -28,13 +62,13 @@ const App = () => {
     setPersons(persons.concat(nameObject))
     console.log('names: ', {persons})
     setNewName('') // clearing the input field
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
@@ -44,19 +78,19 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-      <form onSubmit={handleAddClick}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm 
+      handleNameChange={handleNameChange}
+      handleNumberChange={handleNumberChange}
+      newName={newName}
+      newNumber={newNumber}
+      handleAddClick={handleAddClick}
+      />
 
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
-      {persons.map((person, id) => <p key={id}> {person.name} {person.number}</p>)}
+      <div>
+        filter shown with <input placeholder={'search name'}></input>
+      </div>
+      <Contacts persons={persons} />
     </div>
   )
 
