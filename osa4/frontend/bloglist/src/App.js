00eb4fd4bb8ddda626ab263b46254dfae9bf5bd6
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-11-29 14:49:09 +0200

    Frontend for bloglist app, sketch is ready

--- osa4/frontend/bloglist/src/App.js
+++ osa4/frontend/bloglist/src/App.js
@@ -1,17 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './App.css'
+import blogService from './services/blog'
+import axios from "axios"
+import blog from "./services/blog"
 
 function App() {
   const [blogs, setBlogs] = useState([
     {
       author: 'Nalle Puh',
       title: 'Tarinoita Puolenhehtaarin metsästä',
-      url: 'www.google.com'
+      url: 'www.google.com',
+      likes: 5
     }
   ])
   const [newAuthor, setNewAuthor] = useState('')
   const [newTitle, setNewTitle] = useState('')
   const [newUrl, setNewUrl] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    blogService
+      .getAll()
+      .then(response => {
+        console.log('promise fulfilled')
+        setBlogs(response.data)
+      })
+  }, [])
 
   const handleAddBlogClick = (event) => {
     event.preventDefault();
@@ -20,14 +34,43 @@ function App() {
       author: newAuthor,
       title: newTitle,
       url: newUrl,
+      likes: 0,
     }
 
-    setBlogs(blogs.concat(blogObject))
+    blogService
+    .create(blogObject)
+    .then(response => {
+      console.log('post', response.data)
+      setBlogs(blogs.concat(response.data))
+      setNewAuthor('')
+      setNewTitle('')
+      setNewUrl('')
+    })
     console.log('blogs:', blogs)
-    setNewAuthor('')
-    setNewTitle('')
-    setNewUrl('')
   }
+
+
+  const handleLikeClick = (name) => {
+    console.log('clicked like to', name)
+    const likedBlog = blogs.filter(blog => blog.title===name)
+    console.log('liked blog', likedBlog[0])
+    const likes = likedBlog[0].likes + 1
+    console.log('new likes', likes)
+
+    const changedBlog = {
+      author: likedBlog[0].author,
+      title: likedBlog[0].title,
+      url: likedBlog[0].url,
+      likes: likes
+    }
+
+    blogService
+    .update(likedBlog[0].id, changedBlog)
+    .then(response => {
+      setBlogs(blogs.map(blog => blog.id !== likedBlog[0].id ? blog : response.data))
+    })
+  }
+
 
   const handleAuthorChange = (event) => {
     console.log(event.target.value)
@@ -42,19 +85,25 @@ function App() {
     setNewUrl(event.target.value)
   }
 
+
   return (
     <div className="App">
       <h1>Blogs</h1>
       <p>Add new blog below</p>
       <form onSubmit={handleAddBlogClick}>
-        <input placeholder="Author" value={newAuthor} onChange={handleAuthorChange}></input>
-        <input placeholder="Title" onChange={handleTitleChange}></input>
-        <input placeholder="URL" onChange={handleUrlChange}></input>
-        <button>Add</button>
+        <input placeholder="author" value={newAuthor} onChange={handleAuthorChange}></input>
+        <input placeholder="title" value={newTitle} onChange={handleTitleChange}></input>
+        <input placeholder="url" value={newUrl} onChange={handleUrlChange}></input>
+        <button>add</button>
       </form>
       <h2>All Blogs</h2>
       <div>
-        {blogs.map((blog, id) => <div key={id}> <p><b>{blog.title}</b> by {blog.author}</p> <ul>{blog.url}</ul> </div>)}
+        {blogs.map((blog, id) => <div key={id} className="box"> 
+        <p><b>{blog.title}</b> by {blog.author}</p>
+         <a href={blog.url}>{blog.url}</a>
+         <p className="liketext">Likes {blog.likes}</p>
+         <button className="likebutton" onClick={() => handleLikeClick(blog.title)}>Like ♥</button>
+      </div>)}
       </div>
     </div>
   );
