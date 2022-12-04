fd7c7a296b429e858b005cd1af2fb8ac7e17c687
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-12-04 14:18:59 +0200

    Exercise 4.1 done. Found bugs with FLY LOGS. Now everything works like it should.

--- osa4/backend/index.js
+++ osa4/backend/index.js
@@ -57,7 +57,7 @@ app.post('/api/blogs', (request, response) => {
       })
   })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
