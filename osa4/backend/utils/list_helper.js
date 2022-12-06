504ae112a7dd952dfcaf33d8d65d201d919389ac
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-12-06 11:28:36 +0200

    Exercise 4.4

--- osa4/backend/utils/list_helper.js
+++ osa4/backend/utils/list_helper.js
@@ -3,7 +3,22 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 1) {
+        var blog = blogs.find(blog => blog.likes)
+        return blog.likes
+    } 
+    else { 
+        var totalLikes = blogs.reduce(function (sum, blog) {
+            console.log('hello', sum, blog.likes)
+            return sum + blog.likes
+        }, 0)
+        return totalLikes
+    }
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
