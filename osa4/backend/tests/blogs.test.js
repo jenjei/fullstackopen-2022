9beca86f43559b1dfe492900896bc46e53f6fe60
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-12-06 14:44:12 +0200

    Exercise 4.6*

--- osa4/backend/tests/blogs.test.js
+++ osa4/backend/tests/blogs.test.js
@@ -4,56 +4,56 @@ const listHelper = require('../utils/list_helper')
 // test dataset
 const blogs = [
   {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
     likes: 7,
     __v: 0
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
     __v: 0
   },
   {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
     __v: 0
   },
   {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
     likes: 10,
     __v: 0
   },
   {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
     __v: 0
   },
   {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
     __v: 0
-  }  
+  }
 ]
 
-// testing test, returns one 
+// testing test, returns one
 test('dummy returns one', () => {
   const blogs = []
 
@@ -61,7 +61,7 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-// testing total likes
+// this test returns the sum of likes
 describe('total likes', () => {
   const listWithOneBlog = [
     {
@@ -85,15 +85,26 @@ describe('total likes', () => {
   })
 })
 
-
-test('is the favorite blog', () => {
-  const result = listHelper.favoriteBlog(blogs)
+// this test returns the most liked blog
+describe('is the most liked blog', () => {
+  test('is the all time favorite blog', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    console.log('this is the most liked blog:', result)
     expect(result).toEqual({
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
     })
   })
+})
+
+// this test returns the author with most blogs written
+describe('author with most blogs authored', () => {
+  test('is the most blogs authored', () => {
+    const result = listHelper.authorWithMostBlogs(blogs)
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+})
--- osa4/backend/utils/list_helper.js
+++ osa4/backend/utils/list_helper.js
@@ -1,35 +1,47 @@
 // tests in this file
+var _ = require('lodash')
 
 const dummy = (blogs) => {
-    return 1
+  console.log(blogs)
+  return 1
 }
 
 // this function returns the total sum of likes
 const totalLikes = (blogs) => {
-    if (blogs.length === 1) {
-        var blog = blogs.find(blog => blog.likes)
-        return blog.likes
-    } 
-    else { 
-        var totalLikes = blogs.reduce(function (sum, blog) {
-            console.log('hello', sum, blog.likes)
-            return sum + blog.likes
-        }, 0)
-        return totalLikes
-    }
+  if (blogs.length === 1) {
+    var blog = blogs.find(blog => blog.likes)
+    return blog.likes
+  }
+  else {
+    var totalLikes = blogs.reduce(function (sum, blog) {
+      console.log('hello', sum, blog.likes)
+      return sum + blog.likes
+    }, 0)
+    return totalLikes
+  }
 }
 
 // this function returns the most liked (favorite) blog
 const favoriteBlog = (blogs) => {
-    const favoriteBlog = blogs.reduce(function(prev, current) {
-        console.log('previous', prev, 'current', current)
-        return (prev.likes > current.likes) ? prev : current
-    }) //returns object
-    return favoriteBlog
+  const favoriteBlog = blogs.reduce(function(prev, current) {
+    console.log('previous', prev, 'current', current)
+    return (prev.likes > current.likes) ? prev : current
+  }) //returns object
+  return { title: favoriteBlog.title, author: favoriteBlog.author, likes: favoriteBlog.likes }
+}
+
+// this function returns author with most blogs written
+const authorWithMostBlogs = (blogs) => {
+    var authorsByBlogAmounts = _.countBy(blogs, 'author') // reorganising authors by blog count
+    var mostBlogs = _.max(_.values(authorsByBlogAmounts)) // finding out who has the most blogs authored
+    var author = _.findKey(authorsByBlogAmounts, (key) => key === mostBlogs)
+    console.log('he/she authored most blogs:', author, mostBlogs)
+    return { author: author, blogs: mostBlogs }
 }
 
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  authorWithMostBlogs
 }
