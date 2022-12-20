ab903555e2ec76e30f4072e094d477f9ddf92369
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-12-20 21:33:05 +0200

    Exercise 5.16, unit test for add blog, and eslinted.

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,39 +3,40 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import AddBlogForm from './AddBlogForm'
 
-test('renders title', () => {
-  const blog = {
-    title: 'Benefits of Scrumban',
-    author: 'Kalle Ilves',
-    url: 'www.google.com',
-    likes: 7,
-    user: { 
-        name: 'Jenni',
-        username: 'jenniaylis',
-        password: 'asdf1234'
-    }
-  }
-
-  const user = { // user, handledeleteclick and handlelikeclick is mandatory, because of PropTypes
+const blog = {
+  title: 'Benefits of Scrumban',
+  author: 'Kalle Ilves',
+  url: 'www.google.com',
+  likes: 7,
+  user: {
     name: 'Jenni',
     username: 'jenniaylis',
     password: 'asdf1234'
   }
+}
 
-  const handleDeleteClick = (id) => {
-    console.log('clicked delete', id)
-  }
+const user = {
+  name: 'Jenni',
+  username: 'jenniaylis',
+  password: 'asdf1234'
+}
 
-  const handleLikeClick = (id) => {
-    console.log('clicked delete', id)
-  }
+const handleDeleteClick = (id) => {
+  console.log('clicked delete', id)
+}
 
+const handleLikeClick = (id) => {
+  console.log('clicked delete', id)
+}
+
+test('renders title', () => {
   render (
-    <Blog blog={blog} 
-    user={user} 
-    handleDeleteClick={handleDeleteClick} 
-    handleLikeClick={handleLikeClick} />
+    <Blog blog={blog}
+      user={user}
+      handleDeleteClick={handleDeleteClick}
+      handleLikeClick={handleLikeClick} />
   )
 
   function findElementByText(text) {
@@ -47,7 +48,7 @@ test('renders title', () => {
     }
     return null
   }
-  
+
   const element = screen.getByTestId('not-expanded-title-test') // first way of doing this test
   expect(element).toHaveTextContent('► Benefits of Scrumban')
 
@@ -56,98 +57,71 @@ test('renders title', () => {
 })
 
 
-
-
 test('details are showed when title is clicked', async () => {
-    const blog = {
-        title: 'Benefits of Scrumban',
-        author: 'Kalle Ilves',
-        url: 'www.google.com',
-        likes: 7,
-        user: { 
-            name: 'Jenni',
-            username: 'jenniaylis',
-            password: 'asdf1234'
-        }
-      }
-    
-      const user = {
-        name: 'Jenni',
-        username: 'jenniaylis',
-        password: 'asdf1234'
-      }
-    
-      const handleDeleteClick = (id) => {
-        console.log('clicked delete', id)
-      }
-    
-      const handleLikeClick = (id) => {
-        console.log('clicked delete', id)
-      }
-  
-    const mockHandler = jest.fn()
-  
-    render(
-        <Blog blog={blog} 
-        user={user} 
-        handleDeleteClick={handleDeleteClick} 
-        handleLikeClick={handleLikeClick}
-        test={mockHandler} />
-    )
-  
-    const theUser = userEvent.setup()
-    const title = screen.getByTestId('not-expanded-title-test')
-    await theUser.click(title)
+  const mockHandler = jest.fn()
 
-    const detailTitle = screen.getByTestId('expanded-title-test')
-    expect(detailTitle).toHaveTextContent('▼ Benefits of Scrumban')
-    const author = screen.getByTestId('author-n-username-test')
-    expect(author).toHaveTextContent('Kalle Ilves | added by jenniaylis')
-    const url = screen.getByTestId('url-test')
-    expect(url).toHaveTextContent('www.google.com')
-    const likes = screen.getByTestId('likes-test')
-    expect(likes).toHaveTextContent('7')
-  
-    expect(mockHandler.mock.calls).toHaveLength(1) // this ensures that element has been clicked just once
-  })
+  render(
+    <Blog blog={blog}
+      user={user}
+      handleDeleteClick={handleDeleteClick}
+      handleLikeClick={handleLikeClick}
+      test={mockHandler} />
+  )
 
-  test('clicking the like button twice calls event handler twice', async () => {
-    const blog = {
-        title: 'Benefits of Scrumban',
-        author: 'Kalle Ilves',
-        url: 'www.google.com',
-        likes: 7,
-        user: { 
-            name: 'Jenni',
-            username: 'jenniaylis',
-            password: 'asdf1234'
-        }
-      }
-    
-      const user = {
-        name: 'Jenni',
-        username: 'jenniaylis',
-        password: 'asdf1234'
-      }
-    
-      const handleDeleteClick = (id) => {
-        console.log('clicked delete', id)
-      }
+  const theUser = userEvent.setup()
+  const title = screen.getByTestId('not-expanded-title-test')
+  await theUser.click(title)
+
+  const detailTitle = screen.getByTestId('expanded-title-test')
+  expect(detailTitle).toHaveTextContent('▼ Benefits of Scrumban')
+  const author = screen.getByTestId('author-n-username-test')
+  expect(author).toHaveTextContent('Kalle Ilves | added by jenniaylis')
+  const url = screen.getByTestId('url-test')
+  expect(url).toHaveTextContent('www.google.com')
+  const likes = screen.getByTestId('likes-test')
+  expect(likes).toHaveTextContent('7')
+
+  expect(mockHandler.mock.calls).toHaveLength(1) // this ensures that element has been clicked just once
+})
+
+
+test('clicking the like button twice calls event handler twice', async () => {
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog}
+      user={user}
+      handleDeleteClick={handleDeleteClick}
+      handleLikeClick={mockHandler}
+    />
+  )
+
+  const theUser = userEvent.setup()
+  const button = screen.getByText('♥')
+  await theUser.click(button)
+  await theUser.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
+test('<AddBlogForm /> updates parent state and calls onSubmit', async () => {
+    const user = userEvent.setup()
+    const createBlog = jest.fn()
   
-    const mockHandler = jest.fn()
+    render(<AddBlogForm createBlog={createBlog} />)
   
-    render(
-        <Blog blog={blog} 
-        user={user} 
-        handleDeleteClick={handleDeleteClick} 
-        handleLikeClick={mockHandler}
-        />
-    )
+    const inputAuthor = screen.getByPlaceholderText('author')
+    const inputUrl = screen.getByPlaceholderText('url')
+    const inputTitle = screen.getByPlaceholderText('title')
+    const sendButton = screen.getByText('add')
   
-    const theUser = userEvent.setup()
-    const button = screen.getByText('♥')
-    await theUser.click(button)
-    await theUser.click(button)
+    await user.type(inputAuthor, 'me')
+    await user.type(inputUrl, 'myself')
+    await user.type(inputTitle, 'and AI')
+    await user.click(sendButton)
   
-    expect(mockHandler.mock.calls).toHaveLength(2)
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0].author).toBe('me')
+    expect(createBlog.mock.calls[0][0].url).toBe('myself')
+    expect(createBlog.mock.calls[0][0].title).toBe('and AI')
   })
