4bcaedf9c856bcb06563a301f829bc7cccf8569a
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-12-21 18:24:43 +0200

    Exercise 5.18, tests for login

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog ', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+        name: 'Jenni',
+        username: 'jenniaylis',
+        password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3001')
   })
 
@@ -8,11 +14,22 @@ describe('Blog ', function() {
     cy.contains('Login')
   })
 
-  it('user can log in', function() {
-    cy.get('#username').type('jenniaylis') // Timed out retrying after 4000ms: Expected to find element: #username, but never found it.
-    cy.get('#password').type('asdf1234')
-    cy.contains('login').click()
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+        cy.get('input:first').type('jenniaylis') // with #username -> Timed out retrying after 4000ms: Expected to find element: #username, but never found it.
+        cy.get('input:last').type('salainen')
+        cy.contains('login').click()
+    
+        cy.contains('Jenni logged in')
+    })
 
-    cy.contains('Jenni logged in')
+    it('fails with wrong credentials', function() {
+        cy.get('input:first').type('mluukkai')
+        cy.get('input:last').type('salainen')
+        cy.contains('login').click()
+    
+        cy.contains('wrong username or password')
+        // TODO LATER bonus exercise: Check that the notification shown with unsuccessful login is displayed red.
+    })
   })
 })
