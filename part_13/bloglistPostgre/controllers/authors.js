1fe2913da7055fc599e8c8c2d94d5f395e6f5042
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2023-03-16 17:34:22 +0200

    Exercise 13.16, get authors route with blog and like amount

---
+++ part_13/bloglistPostgre/controllers/authors.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const { Blog } = require('../models/index')
+const { Op, sequelize } = require('../utils/db')
+
+router.get('/', async (req, res) => {
+
+    const authors = await Blog.findAll({
+        attributes: [
+            'author',
+            [sequelize.fn('COUNT', sequelize.col('*')), 'blogs'],
+            'likes',
+        ],
+        group:['author', 'likes'],
+        order: [
+            [sequelize.fn('max', sequelize.col('likes')), 'DESC']
+        ]
+     })
+    
+     if (authors) {
+        res.json(authors)
+     } else {
+        throw Error('Database empty')
+     }
+})
+
+module.exports = router
